c2ba175de863c0f9a9a6d65a6ec7cb1e66ee6530
uomma <marcelloleone975@gmail.com>
2023-03-21 17:35:48 +0100

    api

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const{createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            message: 'messaggio'
+    data() {
+        return {
+         
+          emails:[]
         }
+    },
+
+    beforeMount() {
+
+        for (let i = 1; i <= 10; i++) {     
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+
+                    this.emails.push(response.data.response);
+                });
+        }       
+
     }
 }).mount('#app')
